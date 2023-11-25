444492ea64c97c663bb8dcacb224f4daeadcdabf
LHJ <lhj7234@naver.com>
2023-11-25 22:08:01 +0900

    bj231125

--- baekjoon/baekjoon.py
+++ baekjoon/baekjoon.py
@@ -27,3 +27,23 @@ for i in b:
     count += 1
 
 print(count)
+
+# 10871
+
+n, x = map(int, input().split())
+
+l1 = list(map(int, input().split()))
+
+for i in range(n) :
+    if l1[i] < x :
+        print(l1[i], end = " ")
+    else :
+      continue
+
+# 10818
+
+n = int(input())
+
+l1 = list(map(int, input().split()))
+
+print(min(l1), max(l1))
