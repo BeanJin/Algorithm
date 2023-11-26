c8785225a44f3b6a43d56fbacab52fc20d349402
LHJ <lhj7234@naver.com>
2023-11-27 00:33:22 +0900

    231127bj

--- baekjoon/baekjoon.py
+++ baekjoon/baekjoon.py
@@ -47,3 +47,30 @@ n = int(input())
 l1 = list(map(int, input().split()))
 
 print(min(l1), max(l1))
+2562
+arr = []
+
+for i in range(9) :
+    a = int(input())
+    arr.append(a)
+
+maxarr = int(max(arr))
+print(maxarr)
+
+b = arr.index(maxarr)
+
+print(b + 1)
+
+# 10810
+
+n, m = map(int, input().split())
+
+l1 = [0] * (n + 1)
+
+for a in range(m) :
+  i, j, k = map(int, input().split())
+  for b in range(i, j+1) :
+    l1[b] = k
+
+for c in range(1, len(l1)) :
+  print(l1[c], end = ' ')
